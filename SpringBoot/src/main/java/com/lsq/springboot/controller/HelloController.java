41d1ffb644f838ffd4a0d591aac8cadeb12197c4
Tame-complexity <linshengqian@outlook.com>
2022-01-02 19:03:24 +0800

    .

--- SpringBoot/src/main/java/com/lsq/springboot/controller/HelloController.java
+++ SpringBoot/src/main/java/com/lsq/springboot/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController     //@RestController 的意思就是 Controller里面的方法都以 json 格式输出，不用再写什么 jackjson 配置的了！
 public class HelloController {
-    @RequestMapping("/hello")
+    @RequestMapping("/k8s")
     public String hello() {
         return "Hello Kubernetes";
     }
